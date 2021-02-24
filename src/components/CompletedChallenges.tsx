71151d3f819f9a59c2a79379dc5bad64282339bb
hedenicapedrosa <hedenica@hotmail.com>
2021-02-24 09:26:35 -0300

    ✨  completedChallenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+const CompletedChallenges = () => {
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafios completos</span>
+      <span>05</span>
+    </div>
+  )
+}
+
+export default CompletedChallenges
