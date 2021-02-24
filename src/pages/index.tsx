cd9da69faff247a590d6609bd93a2a32ecaec8e3
hedenicapedrosa <hedenica@hotmail.com>
2021-02-24 09:28:09 -0300

    💄  update global and home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
-import Head from 'next/head'
+import CompletedChallenges from '../components/CompletedChallenges';
+import Countdown from '../components/Countdown';
 import ExperienceBar from '../components/ExperienceBar';
+import Profile from '../components/Profile';
+
+import Head from 'next/head'
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
-    <ExperienceBar />
-  </div>
+    <div className={styles.container}>
+      <Head>
+        <title>Move it!</title>
+      </Head>
+      <ExperienceBar />
+      
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+        </div>
+      </section>
+    </div>
   )
 }
