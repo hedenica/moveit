128deffdf6c0aca30afac1e6e8622a2e9157d304
hedenicapedrosa <hedenica@hotmail.com>
2021-02-27 14:00:18 -0300

    ✨  level up modal component

---
+++ src/components/LevelUpModal.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/LevelUpModal.module.css'
+
+const LevelUpModal = () => {
+  const { level, closeLevelUpModal } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <header>{level}</header>
+
+        <strong>Parabéns</strong>
+        <p>Você alcançou um novo nível</p>
+
+        <button type="button" onClick={closeLevelUpModal}>
+          <img src="/icons/close.svg" alt="Fechar modal"/>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default LevelUpModal
